fe0c94a6571e37c38109bc7f1fd97c6da25c368f
jokumer <service@enobe.de>
2020-01-18 08:14:48 +0000

    Apply fixes from StyleCI

--- Configuration/TCA/Overrides/sys_dmail_group.php
+++ Configuration/TCA/Overrides/sys_dmail_group.php
@@ -1,13 +1,14 @@
 <?php
+
 defined('TYPO3_MODE') || die('Access denied.');
 
 /**
  * Extend TCA for direct_mail recipients by adding a custom item to attach own query using hook 'cmd_compileMailGroup'
- * HOOK must be registered for direct_mail modules 2 & 3 (2 = dmail, 3 = recipient_list)
+ * HOOK must be registered for direct_mail modules 2 & 3 (2 = dmail, 3 = recipient_list).
  */
 // Add type item
 $GLOBALS['TCA']['sys_dmail_group']['columns']['type']['config']['items'][] = [
-    'LLL:EXT:xt_directmail/Resources/Private/Language/locallang.xml:sys_dmail_group.type.I.5', '5'
+    'LLL:EXT:xt_directmail/Resources/Private/Language/locallang.xml:sys_dmail_group.type.I.5', '5',
 ];
 // Add type/showitem
 $GLOBALS['TCA']['sys_dmail_group']['types']['5']['showitem'] =
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,19 @@
 <?php
+
 $EM_CONF['xt_directmail'] = [
-    'title' => 'Directmail recipients - extends EXT:direct_mail',
+    'title'       => 'Directmail recipients - extends EXT:direct_mail',
     'description' => 'Extended configurations for directmail extension. See README.',
-    'category' => '',
-    'author' => 'J. Kummer',
-    'state' => 'stable',
-    'version' => '1.1.0',
+    'category'    => '',
+    'author'      => 'J. Kummer',
+    'state'       => 'stable',
+    'version'     => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3'      => '8.7.0-9.5.99',
             'directmail' => '5.2.0-6.99.99',
         ],
         'conflicts' => [],
-        'suggests' => [],
+        'suggests'  => [],
     ],
     'autoload' => [
         'psr-4' => [
